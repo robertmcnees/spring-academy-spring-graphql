96c4f8a416d516c1b199df119d7a792e73c18a9f
Robert McNees <rmcnees@vmware.com>
2024-03-08 07:24:20 -0500

    Added breadcrumbs for first lesson

---
+++ src/main/java/io/spring/guides/graphqlmusic/tracks/TracksController.java
@@ -0,0 +1,24 @@
+package io.spring.guides.graphqlmusic.tracks;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+@Controller
+public class TracksController {
+
+	private final MongoTemplate mongoTemplate;
+
+	public TracksController(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	//Add GraphQL QueryMapping endpoint
+	
+}

633eb802729d52879bdacd250047820685395f68
Robert McNees <rmcnees@vmware.com>
2024-03-27 09:16:27 -0400

    Added repository method required for testing lab

--- src/main/java/io/spring/guides/graphqlmusic/tracks/TrackRepository.java
+++ src/main/java/io/spring/guides/graphqlmusic/tracks/TrackRepository.java
@@ -3,8 +3,12 @@ package io.spring.guides.graphqlmusic.tracks;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TrackRepository extends MongoRepository<Track, String> {
 
 	List<Track> findByAlbumIdOrderByNumber(String albumId);
+
+	Optional<Playlist> findByAuthorAndNameEquals(String author, String name);
+	
 }
